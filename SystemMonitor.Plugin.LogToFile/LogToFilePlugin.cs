51881529592793fb3f4dd964a1949a94301bbbc9
Yogesh Rathod <yogesh952003@gmail.com>
2025-08-15 18:19:38 +0530

    Add SystemMonitor Plugin to log system usage data to file

---
+++ SystemMonitor.Plugin.LogToFile/LogToFilePlugin.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using SystemMonitor.Core.Interfaces;
+using SystemMonitor.Core.Models;
+using SystemMonitor.Infrastructure.Extensions;
+
+namespace SystemMonitor.Plugin.LogToFile;
+
+/// <summary>
+/// A SystemMonitor plugin that logs system resource usage to a file.
+/// Defaults to <c>{AppContext.BaseDirectory}\log.txt</c> unless overridden by 
+/// the <c>LogToFile:FilePath</c> configuration key.
+/// Creates the file and any missing directories if they do not exist.
+/// Appends new log entries without overwriting existing content.
+/// </summary>
+public class LogToFilePlugin(ILogger<LogToFilePlugin> logger) : IConfigurableSystemMonitorPlugin
+{
+    private string _logFilePath = Path.Combine(AppContext.BaseDirectory, "log.txt");
+    private readonly ILogger _logger = logger;
+
+    public void OnSystemResourceUsageDataReceived(SystemResourceUsageDto systemResourceUsage)
+    {
+        StringBuilder sb = new();
+        _ =sb.AppendLine("------------------------------------------------------------")
+            .AppendLine($"Cpu Usage: {systemResourceUsage.CpuUsage.Used:0.00}%")
+            .AppendLine($"Ram Total: {systemResourceUsage.RamUsage.Total.ToGb()}")
+            .AppendLine($"Ram Used: {systemResourceUsage.RamUsage.Used.ToGb()}")
+            .AppendLine("Disk Usage:");
+        foreach (var diskUsage in systemResourceUsage.DiskUsage)
+        {
+            _ = sb.Append($"  Name: {diskUsage.Name}, Used: {diskUsage.Used.ToGb()}, Total: {diskUsage.Total.ToGb()}");
+        }
+        _ = sb.AppendLine();
+        LogToFilePath(_logFilePath, sb.ToString());
+    }
+
+    /// <summary>
+    /// Configure the plugin
+    /// </summary>
+    /// <param name="config"></param>
+    public void Configure(ISystemMonitorPluginConfig config)
+    {
+        _logFilePath = config.GetConfigValue("LogToFile:FilePath") ?? _logFilePath;
+    }
+
+    /// <summary>
+    /// Logs msg to file.
+    /// Creates file and necessary directories, if they do not exist
+    /// </summary>
+    /// <param name="logFilePath"></param>
+    /// <param name="msg"></param>
+    private void LogToFilePath(string logFilePath, string msg)
+    {
+        var logFileDir = Path.GetDirectoryName(_logFilePath);
+        if (logFileDir is not null && !Directory.Exists(logFileDir))
+        {
+            Directory.CreateDirectory(logFileDir);
+        }
+        File.AppendAllText(logFilePath, msg);
+    }
+}
