16a076cac49bacbd2b3c37f41a189f249f13ad9f
Yogesh Rathod <yogesh952003@gmail.com>
2025-08-15 17:33:36 +0530

    Add SystemMonitor Plugin LogToConsole to log data on console

---
+++ SystemMonitor.Plugin.LogToConsole/LogToConsolePlugin.cs
@@ -0,0 +1,21 @@
+﻿using SystemMonitor.Core.Interfaces;
+using SystemMonitor.Core.Models;
+using SystemMonitor.Infrastructure.Extensions;
+
+namespace SystemMonitor.Plugin.LogToConsole;
+
+public class LogToConsolePlugin : ISystemMonitorPlugin
+{
+    public void OnSystemResourceUsageDataReceived(SystemResourceUsageDto systemResourceUsage)
+    {
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine($"Cpu Usage: {systemResourceUsage.CpuUsage.Used:0.00}%");
+        Console.WriteLine($"Ram Total: {systemResourceUsage.RamUsage.Total.ToGb()}");
+        Console.WriteLine($"Ram Used: {systemResourceUsage.RamUsage.Used.ToGb()}");
+        Console.WriteLine("Disk Usage:");
+        foreach (var diskUsage in systemResourceUsage.DiskUsage)
+        {
+            Console.WriteLine($"  Name: {diskUsage.Name}, Used: {diskUsage.Used.ToGb()}, Total: {diskUsage.Total.ToGb()}");
+        }
+    }
+}
